22a3e32e0c1efd01bd9f1bf6ceca3622413dc203
sese <dev.seseun@gmail.com>
2023-10-05 17:46:22 +0900

    feat: Add chapter3

---
+++ chapter3/shoppingList/script.js
@@ -0,0 +1,65 @@
+const list = document.querySelector('#list-items');
+const input = document.querySelector('#input-name');
+const btnAdd = document.querySelector('#btn-add');
+
+function checkListEmpty() {
+  const noDataText = document.querySelector('#no-data');
+  if (list.children.length === 0) {
+    noDataText.style.display = 'flex';
+    list.style.display = 'none';
+  } else {
+    noDataText.style.display = 'none';
+    list.style.display = 'block';
+  }
+}
+
+function addItem() {
+  if (input.value !== '') {
+    createListItem(input.value);
+    resetInput();
+    checkListEmpty();
+  } else {
+    alert('제품명을 입력하세요.');
+    resetInput();
+  }
+}
+
+function resetInput() {
+  input.value = '';
+  input.focus();
+}
+
+let id = 0;
+function createListItem(value) {
+  const listItem = document.createElement('li');
+  listItem.setAttribute('data-id', id);
+  listItem.innerHTML = `
+      <strong>${value}</strong>
+      <button class="btn-del"><span class="material-icons" data-id=${id}>remove_circle_outline</span></button>
+  `;
+  id++;
+  list.append(listItem);
+  listItem.scrollIntoView({ block: 'center' });
+}
+
+list.addEventListener('click', (e) => {
+  const isBtnDel = e.target.parentNode.classList.contains('btn-del') && e.target.dataset.id;
+  if (isBtnDel) {
+    const toBeDeleted = document.querySelector(`li[data-id="${e.target.dataset.id}"]`);
+    toBeDeleted.remove();
+    checkListEmpty();
+  }
+});
+
+window.addEventListener('load', () => {
+  input.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      // || e.keyCode === 13
+      e.preventDefault();
+      btnAdd.click(); // trigger
+    }
+  });
+  btnAdd.addEventListener('click', () => {
+    addItem();
+  });
+});
